604566108b080121ca4dd8f108ef8c88d0fb3dd5
Hunter1471 <hunt3r1471@gmail.com>
2016-06-19 06:21:05 +0100

    Add files via upload

---
+++ Random Number Generator.py
@@ -0,0 +1,19 @@
+#Made by Hunter1471(hunt3r1471@gmail.com)
+#Imports
+import random
+from random import randint
+
+#Intro
+print("Welcome to the Random Number Generator!")
+
+#Maximum Number
+inputnumber = input("Which number would you like to go up to?")
+maxnumber = int(inputnumber)
+
+#Times Random Numbers Are Generated
+times = input("How many numbers do you want to generate within the range?")
+times = int(times)
+
+#Printing
+for i in range(times):
+    randomnumber = print(randint(1,maxnumber))
